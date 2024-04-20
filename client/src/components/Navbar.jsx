525e501e12c7fdc4cba26f3acffb2df557f7afff
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-04-20 15:32:33 +0530

    navbar changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,34 @@
 import React from "react";
-
+import { signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
+import { db } from "../firebaseConfig";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 const Navbar = () => {
+  const auth = getAuth();
+  // const googleClick=aysnc()=>{
+  //     const provider=new GoogleAuthProvider();
+  //     const result=await signInWithPopup(auth,provider);
+  //     console.log(result);
+  // }
+  const googleClick = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    //console.log(result.user);
+    //const user = result.user;
+
+    //check for user
+
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) {
+      await setDoc(doc(db, "users", user.uid), {
+        name: user.displayName,
+        email: user.email,
+        photoUrl: user.photoURL,
+        timestamp: serverTimestamp(),
+      });
+    }
+  };
   return (
     <>
       <div className="nav_bar">
@@ -8,8 +36,13 @@ const Navbar = () => {
           <h2>Social Media App</h2>
         </div>
         <div className="right">
-          <button className="btn btn-warning mx-5">Login With Google</button>
-          <button className="btn btn-warning ">All Users</button>
+          <button className="btn btn-warning mx-3" onClick={googleClick}>
+            Login With Google
+          </button>
+          <button className="btn btn-warning mx-3">Post</button>
+          <button className="btn btn-warning mx-3">Profile</button>
+          <button className="btn btn-warning mx-3">All Users</button>
+          <button className="btn btn-warning mx-3">Logout</button>
         </div>
       </div>
     </>
---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey: "AIzaSyAj_09zI4XJx1GUGDxTyUtLedCzY4xeKGw",
+  authDomain: "social-media-ae29b.firebaseapp.com",
+  projectId: "social-media-ae29b",
+  storageBucket: "social-media-ae29b.appspot.com",
+  messagingSenderId: "121903988285",
+  appId: "1:121903988285:web:50223258780015bf58b454",
+  measurementId: "G-WPWN2N1G03",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+const storage = getStorage(app);
